58a5bcda30ef0dd12f83d1944554e0c0ee918502
Stanislav Dubinin <stasdubinin@mail.ru>
2025-10-08 05:26:44 +0400

    dinamic conclusion Side advantages

--- src/components/Side/Side.jsx
+++ src/components/Side/Side.jsx
@@ -1,4 +1,6 @@
 import './Side.css';
+import { sideArray } from '../../constants';
+import { Link } from 'react-router-dom';
 
 export const Side = () => {
   return (
@@ -8,49 +10,29 @@ export const Side = () => {
 
         <div className="content-side__box">
           <div className="content-side__list">
-            <div className="content-side__list-item">
-              <img
-                className="content-side__list-item--img"
-                src="/image/side-info-1.svg"
-                alt="side-info-1"
-              />
-              <h5 className="content-side__list-item--title">Доставка</h5>
-              <p className="content-side__list-item--text">
-                Проверка при получении и возможность бесплатно вернуть товар
-              </p>
-            </div>
-            <div className="content-side__list-item">
-              <img
-                className="content-side__list-item--img"
-                src="/image/side-info-2.svg"
-                alt="side-info-2"
-              />
-              <h5 className="content-side__list-item--title">Автотека</h5>
-              <p className="content-side__list-item--text">
-                Отчёт с историей авто: пробег, владельцы, сведения о залоге, ДТП и ремонтах
-              </p>
-            </div>
-            <div className="content-side__list-item">
-              <img
-                className="content-side__list-item--img"
-                src="/image/side-info-3.svg"
-                alt="side-info-3"
-              />
-              <h5 className="content-side__list-item--title">Онлайн-бронирование жилья</h5>
-              <p className="content-side__list-item--text">
-                Посуточная аренда квартир и домов: большой выбор вариантов для поездок по России
-              </p>
-            </div>
+            {sideArray.map(advantages => {
+              return (
+                <div className="content-side__list-item">
+                  <img
+                    className="content-side__list-item--img"
+                    src={advantages.img}
+                    alt={advantages.title}
+                  />
+                  <h5 className="content-side__list-item--title">{advantages.title}</h5>
+                  <p className="content-side__list-item--text">{advantages.description}</p>
+                </div>
+              );
+            })}
           </div>
 
           <div className="content-side__footer">
             <p className="content-side__footer--item">© ООО «Абито», 2011–2021</p>
-            <a href="#!" className="content-side__footer--item">
+            <Link to="/" className="content-side__footer--item">
               Политика конфиденциальности
-            </a>
-            <a href="#!" className="content-side__footer--item">
+            </Link>
+            <Link to="/" className="content-side__footer--item">
               Обработка данных
-            </a>
+            </Link>
           </div>
         </div>
       </div>
